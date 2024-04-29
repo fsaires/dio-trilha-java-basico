ab911f3dc6833bf6fa512188b34db519530c6ff4
Fernando Aires <fsaires@gmail.com>
2024-04-29 08:16:13 -0300

    Criando variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Fernando Aires";
+        System.out.println(meuNome);
+
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
+        int numero = 1;
+        numero = 2;
+        System.out.println(numero);
+
+
+        final double VALOR_PI = 3.14;
+        System.out.println(VALOR_PI);
     }
 }
